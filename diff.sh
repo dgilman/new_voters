efb598eff513a2988e653c1d568fcaa049e55c57
David Gilman <davidgilman1@gmail.com>
2025-05-04 17:40:24 -0400

    sql errors

--- diff.sh
+++ diff.sh
@@ -2,10 +2,10 @@
 
 
 duckdb <<EOF
-CREATE TABLE old_t FROM read_csv('$1', delim = '\t', header = true, ignore_errors = true);
-CREATE TABLE new_t FROM read_csv('$1', delim = '\t', header = true, ignore_errors = true);
+CREATE TABLE old_t AS FROM read_csv('$1', delim = '\t', header = true, ignore_errors = true);
+CREATE TABLE new_t AS FROM read_csv('$1', delim = '\t', header = true, ignore_errors = true);
 
-copy (select * from (select *, false as is_old_t from new_t) new_t2 left join (select *, true as is_old_t from old_t) old_t2   using (ncid) where old_t2.is_old_t is null) to '$3' (format csv, header);
+copy (select * from (select *, false as is_old_t from new_t) new_t2 left join (select *, true as is_old_t from old_t) old_t2   using (ncid) where (old_t2.is_old_t is null) or (old_t2.precinct_abbrv != new_t2.precinct_abbrv)) to '$3' (format csv, header);
 
 
 EOF
