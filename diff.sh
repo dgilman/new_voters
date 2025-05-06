6abd0c861c7989296cc96f9b13b8e8d6ccc94344
David Gilman <davidgilman1@gmail.com>
2025-05-05 20:20:41 -0400

    many silly fixes to diff.sh query

--- diff.sh
+++ diff.sh
@@ -3,12 +3,21 @@
 
 duckdb <<EOF
 CREATE TABLE old_t AS FROM read_csv('$1', delim = '\t', header = true, ignore_errors = true);
-CREATE TABLE new_t AS FROM read_csv('$1', delim = '\t', header = true, ignore_errors = true);
+select count(*) as old_t_cnt from old_t;
+CREATE TABLE new_t AS FROM read_csv('$2', delim = '\t', header = true, ignore_errors = true);
+select count(*) as new_t_cnt from new_t;
 
-copy (select * from (select *, false as is_old_t from new_t) new_t2 left join (select *, true as is_old_t from old_t) old_t2   using (ncid) where (old_t2.is_old_t is null) or (old_t2.precinct_abbrv != new_t2.precinct_abbrv)) to '$3' (format csv, header);
+create view delta_table as
+    select new_t.*
+    from new_t
+    left join old_t
+    using (ncid)
+    where
+        ((old_t.ncid is null) or (old_t.precinct_abbrv != new_t.precinct_abbrv))
+        and
+        new_t.status_cd = 'A';
 
+select count(*) as delta_cnt from delta_table;
 
+copy (select * from delta_table) to '$3' (format csv, header);
 EOF
-
-# XXX omit the is_old and the null columns
-# XXX what happens if someone moves, especially if they move within the county?
