6c4ee43b02a523f2af56d110eddb1cef67a50c55
David Gilman <davidgilman1@gmail.com>
2025-05-27 20:06:54 -0400

    Revert "actually stream the csv rows to fastapi"
    This reverts commit 57615266f09a0b69590555969ab0eede590e6e2f.

--- api.py
+++ api.py
@@ -8,7 +8,7 @@ import enum
 
 import duckdb
 from fastapi import FastAPI, Depends, Query
-from fastapi.responses import StreamingResponse
+from fastapi.responses import FileResponse, StreamingResponse
 import pydantic
 
 app = FastAPI()
@@ -88,19 +88,10 @@ def _csv_route(
 
     db.execute(query)
 
-    def _csv_writer():
-        while True:
-            rows = db.fetchmany(1000)
-            if not rows:
-                return
-            writer.writerows(rows)
-            yield buffa.getvalue()
-
-            buffa.truncate(0)
-            buffa.seek(0)
-
+    writer.writerows(db.fetchall())
+    buffa.seek(0)
     return StreamingResponse(
-        _csv_writer(),
+        buffa,
         media_type="text/csv",
         headers={
             "Content-Disposition": f"attachment;filename={filename}",
